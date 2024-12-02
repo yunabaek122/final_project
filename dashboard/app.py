0a5bb7d2df71da58106961234ba951234388640d
sumnerper <164083998+sumnerper@users.noreply.github.com>
2024-12-01 19:25:31 -0600

    12.1 updates

--- dashboard/app.py
+++ dashboard/app.py
@@ -18,21 +18,37 @@ clean_cc = cooling_centers[(cooling_centers["USER_Open_"] == "All") & (~cooling_
 
 #Create the CC type options  
 cc_types = clean_cc["USER_Type"].unique().tolist()
-types_select = ["All"] + cc_types
+types_select = cc_types
 
 #Create a color map for each CC type option
 color_map = plt.get_cmap("tab10")
 type_colors = {c_type: color_map(i / len(cc_types)) for i, c_type in enumerate(cc_types)}
 
+#Create the options for environmental and social factors 
+choro_choices = {
+    "environ": ["Heat Index", "Tree Cover"],
+    "social": ["Low-Income", "POC"]
+}
+
 app_ui = ui.page_sidebar(
     ui.sidebar(
         ui.input_checkbox_group(
             id="center_type",
-            label="Select cooling center type:",
+            label="Select type of center (multiple allowed):",
             choices = types_select,
-            selected=["All"],
+            selected = types_select,
         ),
-        title="Types",
+        ui.input_switch(
+            id="toggle_environ", 
+            label="Environmental Factors", 
+            value=False
+        ),
+        ui.input_switch(
+            id="toggle_social", 
+            label="Social Factors", 
+            value=False
+        ),
+        title="Center Types and Factors"
     ),
     ui.layout_columns(
         ui.card(
@@ -44,53 +60,71 @@ app_ui = ui.page_sidebar(
             ui.card_header("Social Factors"),
             ui.output_plot("social_plot"),
             full_screen=True,
-        )
+        ),
     ),
     ui.include_css(r"C:\Users\12019\OneDrive - The University of Chicago\Documents\GitHub\final_project\dashboard\styles.css"),
     title="Cooling Centers in DC Dashboard",
-    fillable=True,
+    fillable=True
 )
 
 
 def server(input, output, session):
     @reactive.calc
     def center_subset(): 
-        if "All" in input.center_type(): 
+        if ["All"] in input.center_type(): 
             return clean_cc
         else: 
-            return clean_cc[clean_cc["USER_Type"].isin(input.center_type())]
+            selected_types = input.center_type()
+            return clean_cc[clean_cc["USER_Type"].isin(selected_types)]
 
     @render.plot
     def environ_plot():
-        fig, ax = plt.subplots(figsize=(12,10))
-        empty_DC.plot(ax=ax, edgecolor="black", facecolor="none", label = "Wards")
+        if input.toggle_environ(): 
+            fig, ax = plt.subplots(figsize=(12,10))
+            empty_DC.plot(ax=ax, edgecolor="black", facecolor="none", label = "Wards")
 
-        #Make sure CRS is aligned 
-        center_subset_fix = center_subset().to_crs(empty_DC.crs)
+            selected_df = center_subset()
 
-        #Plot with conditional coloring 
-        if "All" in input.center_type(): 
-            for c_type, color in type_colors.items(): 
-                subset = center_subset_fix[center_subset_fix["USER_Type"] == c_type]
-                subset.plot(ax=ax, alpha=0.7, color=color, marker="o", label=c_type, legend=True)
+            #Make sure CRS is aligned 
+            selected_df_fix = selected_df.to_crs(empty_DC.crs)
 
-        else:
-            for c_type in input.center_type(): 
-                subset = center_subset_fix[center_subset_fix["USER_Type"] == c_type]
-                subset.plot(ax=ax, alpha=0.7, color=type_colors[c_type], marker="o", label=c_type, legend=True)
+            #Plot with conditional coloring 
+            if "All" in input.center_type():
+                for c_type, color in type_colors.items():
+                    subset = selected_df_fix[selected_df_fix["USER_Type"] == c_type]
+                if not subset.empty:
+                    subset.plot(ax=ax, alpha=0.7, color=color, marker="o", label=c_type)
 
-        ax.set_title(f'Cooling Centers by Type', fontsize=14)
-        ax.legend(loc="upper center", bbox_to_anchor=(0.5,-0.1), ncol=1, fontsize="small")
+            else:
+                for c_type in input.center_type(): 
+                    subset = selected_df_fix[selected_df_fix["USER_Type"] == c_type]
+                    subset.plot(ax=ax, alpha=0.7, color=type_colors[c_type], marker="o", label=c_type, legend=True)
 
-        return fig
+            ax.set_frame_on(False)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            ax.legend(loc="upper center", bbox_to_anchor=(0.5,-0.1), ncol=1, fontsize="small")
+
+            return fig
+        else: 
+            return None
 
     @render.plot
     def social_plot(): 
-        return "Empty"
+        if input.toggle_social(): 
+            fig, ax = plt.subplots(figsize=(12,10))
+            empty_DC.plot(ax=ax, edgecolor="black", facecolor="none", label = "Wards")
 
-    @render.data_frame
-    def centers_prop_table(): 
-        return "Test"
+            ax.set_frame_on(False)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
+            return fig 
+        else: 
+            return None
 
 
 app = App(app_ui, server)
